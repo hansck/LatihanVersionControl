49e463cc7eabbdc66c8867e79755506a7ebd6c13
Feli <>
2021-06-16 12:27:08 +0700

    CobaBranch

---
+++ src/main/java/com/mycompany/latihanversioncontrol/students/Feli.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.latihanversioncontrol.students;
+
+/**
+ *
+ * @author Feliciana Gunadi
+ */
+public class Feli {
+    public Feli() {
+        System.out.println("Feli");
+    }
+}
