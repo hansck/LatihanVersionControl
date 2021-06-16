2381d4c09730d77a919cb3ac8285948579820ed2
User <User@DESKTOP-M0J2S6A>
2021-06-16 12:45:18 +0700

    nama

---
+++ src/main/java/com/mycompany/latihanversioncontrol/students/Jordan.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.latihanversioncontrol.students;
+
+/**
+ *
+ * @author User
+ */
+public class Jordan {
+    Jordan(){
+        System.out.println("Jordan");
+    }
+}
