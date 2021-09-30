ca0273145ea00d02b5683dec4a7f3b36ba7c2680
Asus <Asus@LAPTOP-2KDOGGR9>
2021-09-30 22:55:48 +0700

    Latihan Git

---
+++ src/main/java/com/mycompany/latihanversioncontrol/students/Timotius.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.latihanversioncontrol.students;
+
+/**
+ *
+ * @author Asus
+ */
+public class Timotius {
+    public Timotius(){
+        System.out.println("Timotius-1115037");
+    }
+}
