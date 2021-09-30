3cf52ce93a5830469a512b112b0e208ebbae9422
learngamedevelopment <adithiatirtaz@gmail.com>
2021-09-30 12:01:12 +0700

    Add Feature Adit

---
+++ src/main/java/com/mycompany/latihanversioncontrol/students/Adithia.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.latihanversioncontrol.students;
+
+/**
+ *
+ * @author Asus
+ */
+public class Adithia {
+    private void Hello()
+    {
+        System.out.println("Hahah");
+    }
+}
