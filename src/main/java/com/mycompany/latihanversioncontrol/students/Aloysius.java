79aab1d994441db01d4f2255212f0e425625acf1
Yoan Colly <Yoan Colly@DESKTOP-8MB9M6C>
2021-09-30 12:09:38 +0700

    print aloy

--- src/main/java/com/mycompany/latihanversioncontrol/students/Aloysius.java
+++ src/main/java/com/mycompany/latihanversioncontrol/students/Aloysius.java
@@ -4,5 +4,6 @@ public class Aloysius {
     public static void main(String[] args) {
         System.out.println("oi");
         System.out.println("aye");
+        System.out.println("Aloy");
     }
 }
