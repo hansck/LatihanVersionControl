5434dac13f9557d06c918b4e77869dd82aa1fe3d
Wilson <Wilson@LAPTOP-FT650TN2>
2021-09-30 12:07:04 +0700

    Halo

---
+++ src/main/java/com/mycompany/latihanversioncontrol/students/Wilson.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.latihanversioncontrol.students;
+
+/**
+ *
+ * @author Wilson
+ */
+public class Wilson {
+    public static void main(String[] args) {
+        System.out.println("Bala-bala gehu");
+    }
+}
