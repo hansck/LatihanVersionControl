2596bb0686b135f4022594ad275eaaacf9a360fd
Chris <Chris@LAPTOP-212MJ284>
2021-09-30 11:55:54 +0700

    add chris feature

--- src/main/java/com/mycompany/latihanversioncontrol/students/Chris.java
+++ src/main/java/com/mycompany/latihanversioncontrol/students/Chris.java
@@ -11,6 +11,6 @@ package com.mycompany.latihanversioncontrol.students;
  */
 public class Chris {
     public static void main(String[] args) {
-        System.out.println("");
+        System.out.println("Hello world");
     }
 }
