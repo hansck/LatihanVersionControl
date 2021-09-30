cf1caf2237ff7371c279fedc6665dd0568d21779
Hans CK <hans.christian25@gmail.com>
2021-09-30 11:57:32 +0700

    add dewi feature

--- src/main/java/com/mycompany/latihanversioncontrol/students/Dewi.java
+++ src/main/java/com/mycompany/latihanversioncontrol/students/Dewi.java
@@ -1,16 +1,17 @@
+package com.mycompany.latihanversioncontrol.students;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.mycompany.latihanversioncontrol.students;
 
 /**
  *
  * @author hanschristian
  */
 public class Dewi {
-    public static void main(String[] args) {
+    public  Dewi() {
         System.out.println("Dewi");
     }
 }
