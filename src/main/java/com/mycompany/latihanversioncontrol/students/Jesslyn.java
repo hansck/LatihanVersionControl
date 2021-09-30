4ce15855bfdfd2b5a5b6d123fbbe6d16206d0b8e
Jesslyn <Jesslyn@MSI>
2021-09-30 12:10:24 +0700

    add Jesslyn

--- src/main/java/com/mycompany/latihanversioncontrol/students/Jesslyn.java
+++ src/main/java/com/mycompany/latihanversioncontrol/students/Jesslyn.java
@@ -11,6 +11,6 @@ package com.mycompany.latihanversioncontrol.students;
  */
 public class Jesslyn {
     public static void main(String[] args) {
-        System.out.println("Haloo");
+        System.out.println("Haloooo");
     }
 }
