7f46dadcb2d3bb0bc5c7f1978d1b4fc2a464cb8f
may <maycellineselvyanti@gmail.com>
2021-09-30 11:55:22 +0700

    feature maycelline

---
+++ src/main/java/com/mycompany/latihanversioncontrol/students/Maycelline.java
@@ -0,0 +1,7 @@
+package com.mycompany.latihanversioncontrol.students;
+
+public class Maycelline {
+    public static void main(String[] args) {
+        System.out.println("Hai kooooooooo");
+    }
+}
