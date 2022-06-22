1ede60600b4c346e1efa76403a85d52ee664a8f2
JONATHAN <JONATHAN@LAPTOP-A7HU1DOD>
2022-06-22 09:13:14 +0700

    feature joa

---
+++ src/main/java/com/mycompany/latihanversioncontrol/students/Joa.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.latihanversioncontrol.students;
+
+/**
+ *
+ * @author JONATHAN
+ */
+public class Joa {
+
+    public void hola() {
+        System.out.println("apa bahasa inggrisnya nakal?");
+    }
+}
