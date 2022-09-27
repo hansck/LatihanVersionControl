fb1337af6a1c394a706e0bf0b0166abf6c0d9c16
FriendlySB <>
2022-09-27 09:00:29 +0700

    Test Branch

---
+++ src/main/java/class2022/FriendlySB.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package class2022;
+
+/**
+ *
+ * @author USER
+ */
+public class FriendlySB {
+
+    public FriendlySB() {
+        System.out.println("Sakatonik ABC");
+    }
+
+    
+}
