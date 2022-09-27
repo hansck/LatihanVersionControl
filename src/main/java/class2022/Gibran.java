3cb90230d646d3451d5498ffd9a31d102ed65a8a
Galang ikrar <galangikrrs@gmail.com>
2022-09-27 08:55:52 +0700

    ini coba iseng

---
+++ src/main/java/class2022/Gibran.java
@@ -0,0 +1,12 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package class2022;
+
+public class Gibran {
+    public static void main(String[] args) {
+        System.out.println("asas");
+    }
+}
