7821679dc08963377627230388a60352e05bbbf3
calvi <calvi@LAPTOP-7Q8TNS4V>
2022-09-27 09:10:31 +0700

    Merge origin/master
    Conflicts:
    	src/Controller/CustomerController.java
    	src/Controller/LoginController.java
    	src/Controller/StaffController.java
    	src/Main/Main.java
    	src/View/Booking.java
    	src/View/CheckSaldo.java
    	src/View/Login.java
    	src/View/StaffMainMenu.java

---
+++ src/main/java/class2022/calvin.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package class2022;
+
+/**
+ *
+ * @author calvi
+ */
+public class calvin {
+    public static void main(String[] args) {
+        System.out.println("Halo Calvin");
+    }
+}
