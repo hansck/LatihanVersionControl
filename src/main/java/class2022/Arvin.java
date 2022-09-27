eb575b7ce0580e6ee89476aa89263347d72275ce
Arvin Situmorang <Arvin Situmorang@P1J92D9K-ARVIN-TUF-GAMING>
2022-09-27 08:55:10 +0700

    arvin commit

---
+++ src/main/java/class2022/Arvin.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package class2022;
+
+/**
+ *
+ * @author Arvin Situmorang
+ */
+public class Arvin {
+
+    public static void main(String[] args) {
+        System.out.println("github adalah sebuah version control system");
+    }
+}
