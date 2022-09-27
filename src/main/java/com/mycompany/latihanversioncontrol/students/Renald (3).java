85980ca5523d44e1198a63d22ca621e12d32ea18
limtu <limtu@DESKTOP-TG1L28O>
2022-09-27 08:43:12 +0700

    Add jericho2022

--- src/main/java/com/mycompany/latihanversioncontrol/students/Renald (3).java
+++
@@ -1,7 +0,0 @@
-
-public class Renald {
-    public static void main(String[] args) {
-        System.out.println("Renald");
-    }
-    
-}
---
+++ src/main/java/jericho2022.java
@@ -0,0 +1,7 @@
+
+public class jericho2022 {
+
+    public static void main(String[] args) {
+        System.out.println("nama");
+    }
+}
