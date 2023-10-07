8737fc73647209d4680f915d16aa8cb0b2983837
Martin <>
2023-10-07 11:04:13 +0700

    Martin

---
+++ src/main/java/class2023/Martin.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package class2023;
+
+/**
+ *
+ * @author marti
+ */
+public class Martin {
+    public static void main(String[] args) {
+        System.out.println("Martin");
+    }
+}
