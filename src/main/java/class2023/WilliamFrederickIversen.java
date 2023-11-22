d3cdcdcacf8f0ae24c6108c0f7cbab899f5fc5c0
williamfi76 <wfrederickiv76@gmail.com>
2023-11-22 10:50:13 +0700

    Tambah william lengkap

--- src/main/java/class2023/WilliamFrederickIversen.java
+++ src/main/java/class2023/WilliamFrederickIversen.java
@@ -2,6 +2,6 @@ package class2023;
 
 public class WilliamFrederickIversen {
     public static void main(String[] args) {
-        System.out.println("William");
+        System.out.println("William Frederick Iversen");
     }
 }
