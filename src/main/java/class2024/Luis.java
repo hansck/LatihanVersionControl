cd9770e5e34b09eb84665cba2a87837501a54203
luizz26 <rafhaelluis009.rl@gmail.com>
2024-10-26 10:45:54 +0700

    Create Branch Buat File Luis

---
+++ src/main/java/class2024/Luis.java
@@ -0,0 +1,8 @@
+package class2024;
+
+public class Luis {
+    public static void main(String[] args) {
+        System.out.println("\033c");
+        System.out.println("Kata mamah matkul pbo hrs dapet A :)");
+    }
+}
