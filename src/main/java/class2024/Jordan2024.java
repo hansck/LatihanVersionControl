3eee53eb9f0313f7fdd2f92fd1b22c86ad920c02
jordanemmanuelle <jordanemmanuellee@gmail.com>
2024-10-26 10:38:03 +0700

    nambah file jordan2024

---
+++ src/main/java/class2024/Jordan2024.java
@@ -0,0 +1,7 @@
+package class2024;
+
+public class Jordan2024 {
+    public static void main(String[] args) {
+        System.out.println("semoga saya bisa menjalankan semester 3 dengan baik, amin.");
+    }
+}
