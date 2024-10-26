a2fd550169fdc8acc55c799aaee32719d295cfb3
derelefraim <145255845+derelefraim@users.noreply.github.com>
2024-10-26 10:57:31 +0700

    derelCommit

---
+++ src/main/java/class2024/DerelEfraim2024.java
@@ -0,0 +1,8 @@
+package class2024;
+
+public class DerelEfraim2024 {
+    public static void main(String[] args) {
+        System.out.println("PBO A");
+        System.out.println("Aku derel");
+    }
+}
