3a0bfd3cf7d8055985867a613d3fe4cc9abd5f2e
adrianrenardkristanto <kristantoadrian826@gmail.com>
2024-10-26 10:51:03 +0700

    create file Adrian

--- src/main/java/class2024/Adrian2024.java
+++ src/main/java/class2024/Adrian2024.java
@@ -3,6 +3,6 @@ package class2024;
 public class Adrian2024 {
     
     public static void main(String[] args) {
-        System.out.println("Hello Ko Dion ");
+        System.out.println("Hello Ko Hans ");
     }
 }
