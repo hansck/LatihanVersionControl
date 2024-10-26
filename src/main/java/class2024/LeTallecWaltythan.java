df799120e01f62d6d47fd3637467a947d115a4de
Tythan <tsaqiif26@gmail.com>
2024-10-26 10:54:11 +0700

    Semoga berkah

--- src/main/java/class2024/LeTallecWaltythan.java
+++ src/main/java/class2024/LeTallecWaltythan.java
@@ -2,6 +2,7 @@ package class2024;
 
 public class LeTallecWaltythan {
     public static void main(String[] args) {
-        System.out.println("1123034_Le Tallec");
+        System.out.println("Cieee ngintip file ak ya:D");
+        System.out.println("IG: @laawt26");
     }
 }
